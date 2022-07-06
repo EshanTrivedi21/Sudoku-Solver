29d59b68da6ad3c8d317e4120b911fe32bbc1699
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-07-06 22:59:18 +0530

    final commit

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -2,77 +2,123 @@
 from tkinter import *
 
 # creating gui root widget
-root =  Tk()
+root = Tk()
+root.title("SUDOKU SOLVER")
+root.configure(background='#202020')
 
-# printing the the sudoku matrix without using numpy
-def puzzle(a):
+# defining an empty sudoku list
+sudoku = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+# heading label
+myLabel = Label(root, text="Fill in Numbers & click Solve ", width=33, font = ('courier', 15, 'bold'), fg="#fff", bg="#202020").grid(row=0, column=0, columnspan= 9, pady=10)
+
+# creating an empty dictionary to store variables 
+var_holder = {}
+
+# creating a function to solve when clicked on the button
+def solveSudoku() :
+
+    # adding inputs to the sudoku list
     for i in range(9):
         for j in range(9):
-            print(a[i][j],end = " ")
-        print()
+            sudoku[i][j] = var_holder['inp' + str(i+1) + str(j)].get()
+    
+    # printing the the sudoku matrix without using numpy
+    def puzzle(a):
+        for i in range(9):
+            for j in range(9):
+                print(a[i][j],end = " ")
+            print()
 
-# creating a function to check the possibility of a correct answer at a particular position 
-def solve(grid, row, col, num):
+    # creating a function to check the possibility of a correct answer at a particular position 
+    def solve(sudoku, row, col, num):
 
-    # does the number appear in the row ?
-    for x in range(9):
-        if grid[row][x] == num:
-            return False
-             
-    # does the number appear in the colomn ? 
-    for x in range(9):
-        if grid[x][col] == num:
-            return False
- 
-    # does the number appear in the given square?
-    startRow = row - row % 3
-    startCol = col - col % 3
-    for i in range(3):
-        for j in range(3):
-            if grid[i + startRow][j + startCol] == num:
+        # does the number appear in the row ?
+        for x in range(9):
+            if int(sudoku[row][x]) == num:
                 return False
 
-    return True
- 
-# creating a function to check the correct answer 
-def Suduko(grid, row, col):
- 
-    if (row == 9 - 1 and col == 9):
+        # does the number appear in the column ?
+        for x in range(9):
+            if int(sudoku[x][col]) == num:
+                return False
+
+        # does the number appear in the 3X3 square box ?
+        startRow = row - row % 3
+        startCol = col - col % 3
+        for i in range(3):
+            for j in range(3):
+                if int(sudoku[i + startRow][j + startCol]) == num:
+                    return False
+
         return True
-    if col == 9:
-        row += 1
-        col = 0
-    if grid[row][col] > 0:
-        return Suduko(grid, row, col + 1)
-    for num in range(1, 9 + 1, 1): 
-     
-        if solve(grid, row, col, num):
-            grid[row][col] = num
-            if Suduko(grid, row, col + 1):
-                return True
-        grid[row][col] = 0
-    return False
 
-# creating a list named as grid with sudoku game input
-grid = [[2, 5, 0, 0, 3, 0, 9, 0, 1],
-        [0, 1, 0, 0, 0, 4, 0, 0, 0],
-        [4, 0, 7, 0, 0, 0, 2, 0, 8],
-        [0, 0, 5, 2, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 9, 8, 1, 0, 0],
-        [0, 4, 0, 0, 0, 3, 0, 0, 0],
-        [0, 0, 0, 3, 6, 0, 0, 7, 2],
-        [0, 7, 0, 0, 0, 0, 0, 0, 3],
-        [9, 0, 3, 0, 0, 0, 6, 0, 4]]
+    # creating a function to check the correct answer 
+    def Suduko(sudoku, row, col):
 
-# printing the grid
-print("\n")
-print("UNSOLVED SUDOKU: ")
-puzzle(grid)
- 
-if (Suduko(grid, 0, 0)):
-    print("\n")
-    print("SOLVED SUDOKU: ")
-    puzzle(grid)
-    print("\n")
-else:
-    print("Solution does not exist:(")
+        if (row == 9 - 1 and col == 9):
+            return True
+        if col == 9:
+            row += 1
+            col = 0
+        if int(sudoku[row][col]) > 0: 
+            return Suduko(sudoku, row, col + 1)
+        for num in range(1, 9 + 1, 1): 
+        
+            if solve(sudoku, row, col, num):
+                sudoku[row][col] = num
+                if Suduko(sudoku, row, col + 1):
+                    return True
+            sudoku[row][col] = 0
+        return False
+
+    # if the sudoku is solvable
+    if (Suduko(sudoku, 0, 0)):
+
+        # printing the resfreshed empty sudoku
+        var_holder_ = {}
+        for _ in range(9):
+            for __ in range(9):
+                var_holder_['inpt' + str(_+1) + str(__)] = Entry(root, width=3, justify=CENTER, font = ('courier', 15, 'bold'), bg="#3cb043")
+                var_holder_['inpt' + str(_+1) + str(__)].grid(row=_+1, column=__, columnspan=1, ipady=5, padx=3, pady=3)
+                var_holder_['inpt' + str(_+1) + str(__)].insert(0, sudoku[_][__])
+        btn = Button(root, text="IT'S SOLVED, AGAIN?", command=init, width=33, font = ('courier', 15, 'bold'), fg="#fff", bg="#000").grid(row=11, column=0, columnspan= 9, pady=10)
+
+    # if the sudoku is not solvable the program hangs
+    else:
+        for _ in range(9):
+            for __ in range(9):
+                var_holder_['inpt' + str(_) + str(__)] = Entry(root, width=3, justify=CENTER, font = ('courier', 15, 'bold'), bg="#ff6961")
+
+# defining an initialisation function to clear the sudoku list
+def init():
+
+    # printing the resfreshed empty sudoku
+    for _ in range(9):
+        for __ in range(9):
+
+            var_holder['inp' + str(_+1) + str(__)] = Entry(root, width=3, justify=CENTER, font = ('courier', 15, 'bold'), bg="#aec6cf")
+            var_holder['inp' + str(_+1) + str(__)].grid(row=_+1, column=__, columnspan=1, ipady=5, padx=3, pady=3)
+            var_holder['inp' + str(_+1) + str(__)].insert(0, 0)
+
+            locals().update(var_holder)
+
+    # refresh button to restart with an empty sudoku list
+    btn = Button(root, text="SOLVE SUDOKU", command=solveSudoku, width=33, font = ('courier', 15, 'bold'), fg="#fff", bg="#000").grid(row=11, column=0, columnspan= 9, pady=10)
+
+# calling the initialisation function
+init()
+
+# solve button which calls the whole function of the sudoku solving code
+btn = Button(root, text="SOLVE SUDOKU", command=solveSudoku, width=33, font = ('courier', 15, 'bold'), fg="#fff", bg="#000").grid(row=11, column=0, columnspan= 9, pady=10)
+
+# creating a while loop to continously show the frame
+root.mainloop()
